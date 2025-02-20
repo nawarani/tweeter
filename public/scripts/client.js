76d48d5bf283638481641179f40012da12578eca
Anika Nawar <nawarani47@gmail.com>
2025-02-20 01:37:27 -0500

    ajax get req and timeago

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,30 +7,6 @@
 $(document).ready(function() {
   console.log("document ready in client.js");
 
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
   
   const createTweetElement = function(tweet) {
     const $tweet = $(`
@@ -44,7 +20,7 @@ $(document).ready(function() {
         </header>
         <section>${tweet.content.text}</section>
         <footer>
-          <span>${tweet.created_at}</span>
+          <span>${timeago.format(tweet.created_at)}</span>
           <div class = "tweet-icons">
             <i class="fa-sharp fa-solid fa-flag"></i>
             <i class="fa-solid fa-retweet"></i>
@@ -62,5 +38,40 @@ $(document).ready(function() {
     }
   };
 
-  renderTweets(data);
+  
+  // form submission with jquery
+  $('.new-tweet form').on("submit", function(event) {
+    event.preventDefault();
+    const data = $(this).serialize();
+    console.log(data);
+    
+    $.ajax({
+      type:"POST",
+      url: '/tweets',
+      data: data,
+      success: function(res) {
+        console.log('Tweet submitted');
+      },
+      error: function(xhr, status, error) {
+        console.log('Error: ', error);
+      }
+    });
+  });
+  
+  
+  // fetching tweets
+  const loadTweets = function() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      dataType: 'json'
+    })
+    .done(function(data) {
+      console.log('get /api/tweets: ', data);
+      renderTweets(data);
+    })
+  };
+
+  loadTweets();
+
 });
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,7 +3,6 @@ $(document).ready(function() {
 
   $("#tweet-text").on('input', function() {
     const charCount = $(this).val().length;
-    console.log("count", charCount);
     const remainingChars = 140 - charCount;
     const counter = $(this).siblings('div').find('.counter');
     counter.text(remainingChars);
